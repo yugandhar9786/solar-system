4421075b509ac4756e1b914247bc98d65f1f752f
Yugandhar Kanaparthi <142428339+yugandhar9786@users.noreply.github.com>
2025-06-03 15:40:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
+    user: 'superuser',
+    pass: 'SuperPassword',
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
